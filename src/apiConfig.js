739dd6ddfe112e87c0ee0af0c3e2af3cad9a07fc
hewinsonj <hewinsonj@gmail.com>
2022-11-07 19:10:10 -0500

    going back

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://darling-semifreddo-2f057a.netlify.app',
+	production: 'https://redbull-api.fly.dev',
 	development: 'http://localhost:8000',
 }
 
